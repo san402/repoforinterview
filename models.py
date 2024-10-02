6b88ce21c8dc9a7d95d805ded6becac3d2efaa80
san402 <81192414+san402@users.noreply.github.com>
2024-10-02 15:36:23 +0300

    Create models.py

---
+++ models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
